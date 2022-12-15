b7e604d5dff3cf3f03a88bb0957750ec500390ba
thomas laugier <thomas.laugier.13@gmail.com>
2022-12-15 12:26:46 +0100

    controllers are all ok

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,15 +5,32 @@ class ListsController < ApplicationController
     end
 
     def show
+        @list = List.find(params[:id])
+        @bookmarks = Bookmark.where("list_id = #{params[:id]}")
     end
 
     def new
+        @list = List.new
     end
 
     def create
+        @list = List.new(list_params)
+        if @list.save
+            redirect_to list_path(@list)
+        else
+            render :new, status: :unprocessable_entity
+        end
     end
 
     def destroy
+        @list = List.find(params[:id])
+        @list.destroy
+        redirect_to lists_path
     end
     
+    private
+
+    def list_params
+        params.require(:list).permit(:name)
+    end
 end
